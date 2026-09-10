010c562cb805d326a2f86afcb841303ed8b25979
Andreyrvdev <andreyrv18@gmail.com>
2026-09-10 00:19:48 -0300

    Feature: Docker compose e docker Files

---
+++ backend/src/test/java/org/backend/prova/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.backend.prova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
