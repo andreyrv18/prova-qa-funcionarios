2bc3c5c8aaf45d3ff06491a31bac59b4c3fc7c02
Andreyrvdev <andreyrv18@gmail.com>
2026-09-13 15:57:56 -0300

    feat: (Backend) Model - para gerar as tabelas iguais do flyway

--- backend/src/main/java/org/backend/prova/model/CargosModel.java
+++ backend/src/main/java/org/backend/prova/model/CargosModel.java
@@ -10,4 +10,43 @@ public class CargosModel {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private @Nullable Long id;
+
+  @Column(name = "codigo_do_cargo", nullable = false, unique = true)
+  private String codigoDoCargo;
+
+  @Column(name = "descricao_do_cargo", nullable = false, unique = true)
+  private String descricaoDoCargo;
+
+  public CargosModel() {}
+
+  CargosModel(@Nullable Long id, String codigoDoCargo, String descricaoDoCargo) {
+    this.id = id;
+    this.codigoDoCargo = codigoDoCargo;
+    this.descricaoDoCargo = descricaoDoCargo;
+  }
+
+  @Nullable
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(@Nullable Long id) {
+    this.id = id;
+  }
+
+  public String getDescricaoDoCargo() {
+    return descricaoDoCargo;
+  }
+
+  public void setDescricaoDoCargo(String descricaoDoCargo) {
+    this.descricaoDoCargo = descricaoDoCargo;
+  }
+
+  public String getCodigoDoCargo() {
+    return codigoDoCargo;
+  }
+
+  public void setCodigoDoCargo(String codigoDoCargo) {
+    this.codigoDoCargo = codigoDoCargo;
+  }
 }
