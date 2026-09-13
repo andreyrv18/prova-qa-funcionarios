29b6596e3f4979a477af0948b20c30511809e977
Andreyrvdev <andreyrv18@gmail.com>
2026-09-13 15:57:20 -0300

    feat: (Backend) Controller - cria controller para buscar todos os cargos

--- backend/src/main/java/org/backend/prova/controller/CargosController.java
+++ backend/src/main/java/org/backend/prova/controller/CargosController.java
@@ -1,8 +1,14 @@
 package org.backend.prova.controller;
 
+import org.backend.prova.dto.CargosDTO;
+import org.backend.prova.model.CargosModel;
 import org.backend.prova.service.CargosService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cargos")
@@ -12,5 +18,12 @@ public class CargosController {
 
   public CargosController(CargosService cargosService) {
     this.cargosService = cargosService;
+  }
+
+  @GetMapping("/list")
+  public ResponseEntity<List<CargosDTO>> getAllCargos() throws Exception {
+    List<CargosDTO> listaTodosCargos = cargosService.listaTodosCargos();
+
+    return ResponseEntity.ok().body(listaTodosCargos);
   }
 }
