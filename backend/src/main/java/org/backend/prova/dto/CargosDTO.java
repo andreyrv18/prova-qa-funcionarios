059529b491df20594d9545e90332e896e6727854
Andreyrvdev <andreyrv18@gmail.com>
2026-09-13 15:59:19 -0300

    feat: (Backend) DTO - para trazer o retorno do cargo sem o campo id, assim padronizando a estrutura

--- backend/src/main/java/org/backend/prova/dto/CargosDTO.java
+++ backend/src/main/java/org/backend/prova/dto/CargosDTO.java
@@ -1,7 +1,33 @@
 package org.backend.prova.dto;
 
+import org.backend.prova.model.CargosModel;
+
 import java.io.Serializable;
 
 public class CargosDTO implements Serializable {
-  public CargosDTO() {}
+    private String codigoDoCargo;
+    private String descricaoDoCargo;
+
+    public CargosDTO() {}
+
+    public CargosDTO(CargosModel cargos) {
+        this.codigoDoCargo = cargos.getCodigoDoCargo();
+        this.descricaoDoCargo = cargos.getDescricaoDoCargo();
+    }
+
+    public String getCodigoDoCargo() {
+        return codigoDoCargo;
+    }
+
+    public void setCodigoDoCargo(String codigoDoCargo) {
+        this.codigoDoCargo = codigoDoCargo;
+    }
+
+    public String getDescricaoDoCargo() {
+        return descricaoDoCargo;
+    }
+
+    public void setDescricaoDoCargo(String descricaoDoCargo) {
+        this.descricaoDoCargo = descricaoDoCargo;
+    }
 }
