a7df8cf1fbbead08e0be7d2319ae88d90631d5bc
Andreyrvdev <andreyrv18@gmail.com>
2026-09-14 07:34:43 -0300

    Feature: (Frontend) interfaces

--- frontend/src/interfaces/Interfaces.ts
+++ frontend/src/interfaces/Interfaces.ts
@@ -1,3 +1,41 @@
-export interface I {
-    string: string;
+export interface IFuncionarios {
+    cpf: string;
+    nome: string;
+}
+
+export interface ICargos {
+    codigoDoCargo: string;
+    descricaoDoCargo: string;
+}
+
+export interface IDepartamentos {
+    codigoDoDepartamento: string;
+    descricaoDoDepartamento: string;
+}
+
+export interface IPageResponse<T> {
+    content: T[];
+    totalPages: number;
+    totalElements: number;
+    size: number;
+    number: number;
+}
+
+export interface IVinculos {
+    cargo: string;
+    departamento: string;
+    empresa: string;
+    funicionario: string;
+    matricula: number;
+}
+
+export interface IVinculoItem {
+    id?: number | string;
+    empresa: string;
+    matricula: string;
+    cargo: string;
+    departamento: string;
+    cpf?: string;
+
+    [key: string]: any;
 }
