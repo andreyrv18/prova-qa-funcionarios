e808a2739623afd23b10414d18209ab3ce7ab22f
Andreyrvdev <andreyrv18@gmail.com>
2026-09-14 07:36:17 -0300

    Feature: (Frontend) Services genericos

--- frontend/src/services/FuncionariosService.ts
+++ frontend/src/services/FuncionariosService.ts
@@ -1,10 +1,69 @@
-import { dicionarioDeRotas } from "../ultil/DicionarioDeRotas.ts";
+import type {IFuncionarios,IPageResponse} from "../interfaces/Interfaces.ts";
+import {API_BASE_URL,dicionarioDeRotas} from "../ultil/DicionarioDeRotas.ts";
 
-export const GetFuncionarios = async (): Promise<[]> => {
+export const GetFuncionariosList = async (): Promise<IFuncionarios[]> => {
     const response = await fetch(dicionarioDeRotas.api.funcionarios, {
         method: "GET",
     });
+
+    if (!response.ok) {
+        throw await response.json();
+    }
+
     const dataJson = await response.json();
     console.info(dataJson);
     return dataJson;
+};
+
+export const GetFuncionariosPaginado = async (): Promise<
+    IPageResponse<IFuncionarios>
+> => {
+    const response = await fetch(dicionarioDeRotas.api.funcionarios, {
+        method: "GET",
+    });
+
+    if (!response.ok) {
+        throw await response.json();
+    }
+
+    const dataJson = await response.json();
+    console.info(dataJson);
+    return dataJson;
+};
+
+export const getFuncionarioById = async (
+    id: string,
+): Promise<IFuncionarios> => {
+    const response = await fetch(`${dicionarioDeRotas.api.funcionarios}/${id}`, {
+        method: "GET",
+    });
+    if (!response.ok) {
+        throw await response.json();
+    }
+    const dataJson = await response.json();
+    console.info(dataJson);
+    return dataJson;
+};
+
+export const PostFuncionarios = async (payload: object) => {
+    const response = await fetch(`${API_BASE_URL}/funcionarios`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload),
+    });
+
+    return await response.json();
+};
+
+export const PutFuncionario = async (cpf: string, funcionarioDTO: object) => {
+    const response = await fetch(`${dicionarioDeRotas.api.funcionarios}/${cpf}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(funcionarioDTO),
+    });
+
+    if (!response.ok) {
+        throw await response.json();
+    }
+    return await response.json();
 };
--- frontend/src/services/VinculosService.ts
+++ frontend/src/services/VinculosService.ts
@@ -1,10 +1,64 @@
-import { dicionarioDeRotas } from "../ultil/DicionarioDeRotas.ts";
+import type {IPageResponse,IVinculos} from "../interfaces/Interfaces.ts";
+import {dicionarioDeRotas} from "../ultil/DicionarioDeRotas.ts";
 
-export const GetVinculos = async (): Promise<[]> => {
-    const response = await fetch(dicionarioDeRotas.api.vinculos, {
+export const GetVinculoByCpf = async (id: string): Promise<IVinculos> => {
+    const response = await fetch(`${dicionarioDeRotas.api.vinculos}/${id}`, {
         method: "GET",
     });
+    if (!response.ok) {
+        throw await response.json();
+    }
     const dataJson = await response.json();
     console.info(dataJson);
     return dataJson;
+};
+
+export const GetVinculoList = async (): Promise<IVinculos[]> => {
+    const response = await fetch(dicionarioDeRotas.api.vinculos, {
+        method: "GET",
+    });
+
+    if (!response.ok) {
+        throw await response.json();
+    }
+
+    const dataJson = await response.json();
+    console.info(dataJson);
+    return dataJson;
+};
+
+export const GetVinculoPage = async (): Promise<IPageResponse<IVinculos>> => {
+    const response = await fetch(dicionarioDeRotas.api.vinculosPage, {
+        method: "GET",
+    });
+
+    if (!response.ok) {
+        throw await response.json();
+    }
+
+    const dataJson = await response.json();
+    console.info(dataJson);
+    return dataJson;
+};
+
+export const PostVinculo = async (payload: object) => {
+    const response = await fetch(dicionarioDeRotas.api.vinculos, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload),
+    });
+
+    if (!response.ok) throw await response.json();
+    return await response.json();
+};
+
+export const PutVinculo = async (id: string | number, payload: object) => {
+    const response = await fetch(`${dicionarioDeRotas.api.vinculos}/${id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload),
+    });
+
+    if (!response.ok) throw await response.json();
+    return await response.json();
 };
