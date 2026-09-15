2a160c82e6869c4eb3778f405917f5b7b420d693
Andreyrvdev <andreyrv18@gmail.com>
2026-09-14 22:06:29 -0300

    fix: (Frontend) Routes - faz o loader corretamente e busca os dados mapeando na interface

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,18 +1,27 @@
-import {createBrowserRouter, Navigate} from "react-router";
+import { createBrowserRouter, Navigate } from "react-router";
 import App from "./App.tsx";
 import PaginaErro from "./pages/PaginaErro.tsx";
 import PaginaFuncionarios from "./pages/funcionarios/PaginaFuncionarios.tsx";
 import PaginaDepartamentos from "./pages/departamentos/PaginaDepartamentos.tsx";
 import PaginaCargos from "./pages/cargos/PaginaCargos.tsx";
-import {getFuncionarioById, GetFuncionariosList} from "./services/FuncionariosService.ts";
-import {dicionarioDeRotas} from "./ultil/DicionarioDeRotas.ts";
+import {
+    getFuncionarioById,
+    GetFuncionariosList,
+} from "./services/FuncionariosService.ts";
+import { dicionarioDeRotas } from "./ultil/DicionarioDeRotas.ts";
 import EditarFuncionarios from "./pages/funcionarios/EditarFuncionarios.tsx";
-import {GetCargos, GetCargosById} from "./services/CargosService.ts";
-import type {ICargos, IDepartamentos, IFuncionarios} from "./interfaces/Interfaces.ts";
-import {GetDepartamentoById, GetDepartamentos} from "./services/DepartamentosService.ts";
+import { GetCargos, GetCargosById } from "./services/CargosService.ts";
+import type {
+    ICargos,
+    IDepartamentos,
+    IFuncionarios,
+} from "./interfaces/Interfaces.ts";
+import {
+    GetDepartamentoById,
+    GetDepartamentos,
+} from "./services/DepartamentosService.ts";
 import EditarCargos from "./pages/cargos/EditarCargos.tsx";
 import EditarDepartamentos from "./pages/departamentos/EditarDepartamentos.tsx";
-import {GetVinculoByCpf} from "./services/VinculosService.ts";
 import CadastrarFuncionarios from "./pages/funcionarios/CadastrarFuncionarios.tsx";
 import CadastrarCargos from "./pages/cargos/CadastrarCargos.tsx";
 import CadastrarDepartamentos from "./pages/departamentos/CadastrarDepartamentos.tsx";
@@ -20,83 +29,112 @@ import CadastrarDepartamentos from "./pages/departamentos/CadastrarDepartamentos
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <App/>,
+        element: <App />,
         HydrateFallback: () => <div>Carregando aplicação...</div>,
-        errorElement: <PaginaErro/>,
+        errorElement: <PaginaErro />,
         children: [
             {
                 index: true,
-                element: <Navigate to={dicionarioDeRotas.cargos.listar} replace/>,
+                element: (
+                    <Navigate to={dicionarioDeRotas.cargos.listar} replace />
+                ),
             },
             {
                 id: "rota-funcionarios",
                 path: dicionarioDeRotas.funcionarios.listar,
-                element: <PaginaFuncionarios/>,
-                loader: async (): Promise<{ records: IFuncionarios[] }> => {
-                    return {records: await GetFuncionariosList()};
+                element: <PaginaFuncionarios />,
+                loader: async (): Promise<{
+                    records: IFuncionarios[];
+                    cargos: ICargos[];
+                    departamentos: IDepartamentos[];
+                }> => {
+                    const [records, cargos, departamentos] = await Promise.all([
+                        GetFuncionariosList(),
+                        GetCargos(),
+                        GetDepartamentos(),
+                    ]);
+                    return { records, cargos, departamentos };
                 },
             },
             {
                 id: "rota-funcionarios-cadastrar",
                 path: dicionarioDeRotas.funcionarios.cadastrar,
-                element: <CadastrarFuncionarios/>,
+                element: <CadastrarFuncionarios />,
+                loader: async (): Promise<{
+                    cargos: ICargos[];
+                    departamentos: IDepartamentos[];
+                }> => {
+                    const [cargos, departamentos] = await Promise.all([
+                        GetCargos(),
+                        GetDepartamentos(),
+                    ]);
+                    return { cargos, departamentos };
+                },
             },
             {
                 id: "rota-funcionarios-editar",
                 path: dicionarioDeRotas.funcionarios.id,
-                element: <EditarFuncionarios/>,
-                loader: async ({params}): Promise<{}> => {
+                element: <EditarFuncionarios />,
+                loader: async ({
+                    params,
+                }): Promise<{
+                    records: IFuncionarios;
+                    cargos: ICargos[];
+                    departamentos: IDepartamentos[];
+                }> => {
                     const cpf = params.id;
                     if (!cpf) throw new Error("CPF não fornecido");
-                    const [funcionario, listaVinculos] = await Promise.all([
-                        getFuncionarioById(cpf),
-                        GetVinculoByCpf(cpf),
-                    ]);
+
+                    const [funcionario, cargos, departamentos] =
+                        await Promise.all([
+                            getFuncionarioById(cpf),
+                            GetCargos(),
+                            GetDepartamentos(),
+                        ]);
+
                     return {
-                        records: {
-                            ...funcionario,
-                            vinculos: listaVinculos,
-                        },
+                        records: funcionario,
+                        cargos,
+                        departamentos,
                     };
                 },
             },
             {
                 id: "rota-cargos",
                 path: dicionarioDeRotas.cargos.listar,
-                element: <PaginaCargos/>,
+                element: <PaginaCargos />,
                 loader: async (): Promise<{ records: ICargos[] }> => {
-                    return {records: await GetCargos()};
+                    return { records: await GetCargos() };
                 },
             },
             {
                 id: "rota-cargos-cadastrar",
                 path: dicionarioDeRotas.cargos.cadastrar,
-                element: <CadastrarCargos/>,
+                element: <CadastrarCargos />,
             },
             {
-                path: dicionarioDeRotas.cargos.id, // ex: "/cargos/:id"
-                element: <EditarCargos/>,
-                loader: async ({params}) => GetCargosById(params.id!),
+                path: dicionarioDeRotas.cargos.id,
+                element: <EditarCargos />,
+                loader: async ({ params }) => GetCargosById(params.id!),
             },
-
             {
                 id: "rota-departamentos",
                 path: dicionarioDeRotas.departamentos.listar,
-                element: <PaginaDepartamentos/>,
+                element: <PaginaDepartamentos />,
                 loader: async (): Promise<{ records: IDepartamentos[] }> => {
-                    return {records: await GetDepartamentos()};
+                    return { records: await GetDepartamentos() };
                 },
             },
             {
                 id: "rota-departamentos-cadastrar",
                 path: dicionarioDeRotas.departamentos.cadastrar,
-                element: <CadastrarDepartamentos/>,
+                element: <CadastrarDepartamentos />,
             },
             {
-                path: dicionarioDeRotas.departamentos.id, // ex: "/departamentos/:id"
-                element: <EditarDepartamentos/>,
-                loader: async ({params}) => GetDepartamentoById(params.id!),
-            }
+                path: dicionarioDeRotas.departamentos.id,
+                element: <EditarDepartamentos />,
+                loader: async ({ params }) => GetDepartamentoById(params.id!),
+            },
         ],
     },
 ]);
