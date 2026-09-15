611d6c10c92ac81ff8ae59afe35816e05a7f4d63
Andreyrvdev <andreyrv18@gmail.com>
2026-09-14 22:55:49 -0300

    ajusta rotas front par ñao gerar conflito com o cors

--- backend/src/main/java/org/backend/prova/config/CorsConfig.java
+++ backend/src/main/java/org/backend/prova/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package org.backend.prova.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -12,10 +13,13 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
+    @Value("${app.cors.origins}")
+    private String[] allowedOrigins;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList(allowedOrigins));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
