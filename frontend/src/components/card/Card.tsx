f4a74db2a68aba88a0fe5b9080440dea73799094
Andreyrvdev <andreyrv18@gmail.com>
2026-09-14 22:02:23 -0300

    feat: (Frontend) Components: Card com css e arquivo de types

--- frontend/src/components/card/Card.tsx
+++ frontend/src/components/card/Card.tsx
@@ -1,11 +1,25 @@
 import "./Card.css";
 import type { CardProps } from "./Card.types";
 
-export const Card = ({ title, children, className = "" }: CardProps) => {
+export const Card: React.FC<CardProps> = ({ title, action, children }) => {
     return (
-        <div className={`dixi-card ${className}`}>
-            {title && <h2 className="dixi-card__title">{title}</h2>}
-            <div className="dixi-card__content">{children}</div>
+        <div className="dixi-card">
+            {(title || action) && (
+                <div
+                    className="dixi-card__header"
+                    style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                    }}
+                >
+                    {title && <h3 className="dixi-card__title">{title}</h3>}
+                    {action && (
+                        <div className="dixi-card__action">{action}</div>
+                    )}
+                </div>
+            )}
+            <div className="dixi-card__body">{children}</div>
         </div>
     );
 };
--- frontend/src/components/card/Card.types.ts
+++ frontend/src/components/card/Card.types.ts
@@ -2,6 +2,6 @@ import React from "react";
 
 export interface CardProps {
     title?: string;
-    children: React.ReactNode;
-    className?: string;
+    action?: React.ReactNode;
+    children?: React.ReactNode;
 }
